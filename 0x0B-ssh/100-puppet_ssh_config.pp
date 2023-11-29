fe44ace5aac8a2f9b3a9986097613a8c8fceeeaa
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-29 15:57:49 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,44 @@
 # Client configuration file (w/ Puppet)
 
-file { '~/.ssh/config':
-  path    => '~/.ssh/config',
-  content => 'Host *\n    PasswordAuthentication no\n    IdentityFile ~/.ssh/school',
+class ssh_config {
+  package { 'augeas-tools':
+    ensure => installed,
+  }
+
+  augeas { 'turn_off_passwd_auth':
+    context => '/files/etc/ssh/ssh_config',
+    changes => [
+      'set Host/*[. = "*"]/PasswordAuthentication no',
+    ],
+    notify  => Service['ssh'],
+  }
+
+  augeas { 'declare_identity_file':
+    context => '/files/etc/ssh/ssh_config',
+    changes => [
+      'set Host/*[. = "*"]/IdentityFile /home/user/.ssh/school',
+    ],
+    notify  => Service['ssh'],
+  }
+
+  file_line { 'Turn off passwd auth':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'PasswordAuthentication no',
+    match   => '^#?\s*PasswordAuthentication',
+    notify  => Service['ssh'],
+  }
+
+  file_line { 'Declare identity file':
+    path    => '/etc/ssh/ssh_config',
+    line    => 'IdentityFile ~/.ssh/school',
+    match   => '^#?\s*IdentityFile',
+    notify  => Service['ssh'],
+  }
+
+  service { 'ssh':
+    ensure  => 'running',
+    enable  => true,
+    require => [Augeas['turn_off_passwd_auth'], Augeas['declare_identity_file']],
+  }
 }
+
