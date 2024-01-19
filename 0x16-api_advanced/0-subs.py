38ad2cf13f4ad05833ac4e4b53101daa4b8a14ad
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-01-18 16:21:46 -0800

    changed str format

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return total number of subscriber for a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'alx-api/0.1 (by u/elghafiani)'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'alx-api-12345/0.1 (by u/elghafiani)'}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        return 0
     json = res.json()
     if json["data"].get("subscribers"):
         return json["data"].get("subscribers")
