872a86f991627ece6df5b5e24fe6cf1adbb373ca
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-01-18 17:26:16 -0800

    fixed issue

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,8 +7,10 @@ def top_ten(subreddit):
     """return top 10 hot posts for a subreddit"""
     h = {'User-Agent': 'MYAPP/1.0 (by /u/elghafiani)'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {'limit': 10, 'after': after} if after else {'limit': 10}
-    res = requests.get(url, headers=h, params=params, allow_redirects=False)
+    res = requests.get(url, headers=h, allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
+        return
     data = res.json()["data"]["children"]
-    for post in data:
+    for post in data[:10]:
         print(post["data"]["title"])
