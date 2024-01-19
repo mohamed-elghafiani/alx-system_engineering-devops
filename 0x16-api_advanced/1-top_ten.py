fa2f70c2fb7c165e3471d88b7cca81f4f6a82a16
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-01-18 16:45:01 -0800

    top 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""API script"""
+import requests
+
+
+def top_ten(subreddit):
+    """return total number of subscriber for a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'alx-api-12345/0.1 (by u/elghafiani)'}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    data = res.json()["data"]["children"]
+    for post in data[:10]:
+        print(post["data"]["title"])
