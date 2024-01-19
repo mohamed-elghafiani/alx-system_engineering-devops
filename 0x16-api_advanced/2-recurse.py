972d06538ddde49f137bc96307ad18d5887ae24e
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-01-18 17:16:42 -0800

    fixed style

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ def recurse(subreddit, hot_list=[], after=None):
     if subreddit is None:
         return None
 
-    h = {'User-Agent': 'MyRedditApp/1.0 (by /u/YourRedditUsername)'}
+    h = {'User-Agent': 'MYAPP/1.0 (by /u/elghafiani)'}
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 100, 'after': after} if after else {'limit': 100}
